582af990cbefe435269649bbb8177e191a23aa4f
Damian Szuster <adverstm@gmail.com>
2023-06-27 10:10:51 +0200

    Cherry pick "Demo code cleanup"

--- iOS_Projekt/TaskViewController.swift
+++ iOS_Projekt/TaskViewController.swift
@@ -11,21 +11,52 @@ import CoreData
 class TaskViewController: UIViewController {
     
     @IBOutlet var label: UILabel!
-    @IBOutlet var descField: UITextView!
     @IBOutlet var imageView: UIImageView!
+    @IBOutlet var descView: UITableView!
     
     var recipe: Recipe!
     var currentPosition: Int?
+    var recipeSteps: [String]!
     
     var update: (()->Void)?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        descView.delegate = self
+        descView.dataSource = self
+        
         label.text = recipe.name
-        descField.text = recipe.desc
+        recipeSteps = recipe.desc!.components(separatedBy: "\n").filter { !$0.isEmpty }
         if let img = recipe.image {
             imageView.image = UIImage(data: img)
         }
+    }
+}
+
+extension TaskViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+}
+
+extension TaskViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipeSteps.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let lbl = cell.contentView.viewWithTag(2) as! UILabel
+        lbl.text = recipeSteps[indexPath.row]
+        
+        let btnChk = cell.contentView.viewWithTag(1) as! UIButton
+        btnChk.addTarget(self, action: #selector(checkboxClicked(_ :)), for: .touchUpInside)
+        
+        return cell
+    }
+    
+    @objc func checkboxClicked(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
     }
 }
